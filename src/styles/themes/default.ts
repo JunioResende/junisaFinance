1415aa3baf465b1e87a88aceece40bbbcf428796
JunioResende <junioresende23@gmail.com>
2022-10-23 19:56:50 -0300

    defaultTheme and fonts

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,64 @@
+export const defaultTheme = {
+  // white
+  white: '#FFF',
+
+  // black
+  black: '#000',
+
+  // gray scale
+  'gray-100': '#E1E1E6',
+  'gray-300': '#C4C4CC',
+  'gray-400': '#8D8D99',
+  'gray-500': '#7C7C8A',
+  'gray-600': '#323238',
+  'gray-700': '#29292E',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  // red
+  'red-100': '#D9534F',
+  'red-300': '#C34B47',
+  'red-500': '#AE423F',
+  'red-700': '#983A37',
+  'red-900': '#82322F',
+
+  // orange
+  'orange-100': '#FF6600',
+  'orange-700': '#A34100',
+
+  // yellow
+  'yellow-100': '#f0ad4e',
+  'yellow-300': '#D89C46',
+  'yellow-500': '#C08A3E',
+  'yellow-700': '#A87937',
+  'yellow-900': '#90682F',
+
+  // pink
+  'pink-300': '#F884DC',
+  'pink-900': '#F21FBF',
+
+  // violet
+  'violet-100': '#D299FF',
+  'violet-300': '#C173FF',
+  'violet-500': '#B14DFF',
+  'violet-700': '#A026FF',
+  'violet-900': '#8F00FF',
+
+  // blue
+  'blue-100': '#0275D8',
+  'blue-300': '#0269C2',
+  'blue-500': '#025EAD',
+  'blue-700': '#015297',
+  'blue-900': '#014682',
+
+  // green
+  'green-100': '#5CB85C',
+  'green-300': '#53A653',
+  'green-500': '#4A934A',
+  'green-700': '#408140',
+  'green-900': '#376E37',
+
+  // cyan
+  'cyan-100': '#5BC0DE',
+  'cyan-900': '#377385',
+}
